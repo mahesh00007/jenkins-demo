c42ad6fe6e8fa5b68ceb2b48a82ceba1871d3672
mahesh <mkella@red-shift.us>
2024-12-03 14:01:52 +0530

    changed the sysout

--- src/main/java/com/eb/poc/PocApplication.java
+++ src/main/java/com/eb/poc/PocApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PocApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Mahesh");
+		System.out.println("Mahesh Kella");
 		SpringApplication.run(PocApplication.class, args);
 	}
 
